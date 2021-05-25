785f1bc6edab79e7c9aff863c94efc465a6de453
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-05-26 00:54:47 +0100

    Added Tasks class basic structure'

---
+++ src/task.js
@@ -0,0 +1,37 @@
+class Task {
+  constructor(name, description = '', dueDate) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.completed = false;
+  }
+
+  set name(newName) {
+    this.name = newName;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set description (newDescription) {
+    this.description = newDescription;
+  }
+
+  get description() {
+    return this.description;
+  }
+
+  set dueDate(newDueDate) {
+    this.dueDate = newDueDate;
+  }
+
+  get dueDate() {
+    return this.dueDate;
+  }
+
+  toggleCompletion(task) {
+    task.completed = !task.completed;
+  }
+
+}
